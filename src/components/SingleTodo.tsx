0b9ebcc59e6d80337fd6610f4408d5cad882c199
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-04-12 07:22:12 -0400

    moving along

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Todo } from "./model";
 
-const SingleTodo = () => {
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
   return (
-    <div>SingleTodo</div>
-  )
-}
+    <form className="todos__single">
+      <span className="todos__single--text">{todo.todo}</span>
+    </form>
+  );
+};
 
-export default SingleTodo
+export default SingleTodo;
